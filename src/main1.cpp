96def6930e5e8cf6acd7918a23b4fdb134922b39
zmw24 <zmw24@pitt.edu>
2025-10-27 22:07:47 -0400

    Added comments outlining a list of requirements for each step of the issue/ex/mem/wb/commit process

--- src/main1.cpp
+++ src/main1.cpp
@@ -23,6 +23,7 @@ int num_addi=0;
 int num_addf=0;
 int num_mulf=0;
 int num_mem=0;
+int currentCycle = 0;
 
 // External globals declared in input_parser.cpp
 extern int num_CDB_buf;
@@ -68,9 +69,10 @@ int main()
 
 
 
-	// I'm not sure if we need a separate int and float CDB...?
+	// I'm not sure if the CDB should be int or float...?
     CDB<int> intCDB(num_CDB_buf);
     CDB<float> floatCDB(num_CDB_buf);
+
 
 	// Example: push a value to the CDB
     std::cout << "Pushing value 42 to integer CDB..." << std::endl;
@@ -97,17 +99,59 @@ int main()
     
 
 	std::cout << "Begin Tomasulo algorithm" << std::endl;
+
+
 	// ...
 	// The actual issue/ex/mem/wb/commit process happens here
 	// ...
+    while (the last instruction has not been committed)
+    {
+
+        currentCycle++;
+
+        // We could consider creating a new file for each of the functions below if there will be a lot of helper functions,
+        // or maybe just one with everything.
+        issue();
+            // Fetch and decode instruction from instruction queue (record issue cycle for table)
+            // Check if there are free RS for instruction and ROB entry (stall if not available), then allocate for new instruction
+            // Read operands in registers (if not available yet, record which RS will eventually produce the result)
+            // Register renaming, update RAT with source/target for new ROB entry
+            // Branch prediction will eventually happen here.
+        execute();
+            // Execute instructions once all operands are ready (record execute cycle for table)
+            // "Compete" to use functional unit (oldest first)
+            // Advance instruction count for each instruction in execution. Wait for the number of cycles read from the input before marking the instruction as ready (cycle_addi, cycle_addf, etc.)
+            // When an FU's execution completes, mark the RS entry as 'finished' so that it can be written on the next writeback cycle
+            // For load/store, also calculate effective address (does not occupy integer ALU to do this).
+            // Eventually, branch resolution will happen here
+        mem();
+            // Record mem cycle for table
+            // Check for "forwarding-from-a-store" as mentioned in project document. It takes 1 cycle to perform the forwarding if a match is found. If not found then the load accesses the memory for data.
+            // Once a load returns from the memory or gets the value from a previous store, its entry in the load/store queue is cleared.
+            // Note that it is correct not to clear this entry, but the queue can quickly fill up, causing structure hazards for future loads/stores.
+        writeback();
+            // Broadcast results onto the CDB (other instructions waiting on it will need to pick it up), or buffer if the CDB is full
+            // Write result back to RS and ROB entry (record WB cycle for table)
+            // Mark the ready/finished bit in ROB since the instruction has completed execution
+            // Free reservation stations for future reuse when finished
+            // Store instructions write to memory in this stage
+        commit();
+            // Commit an instruction when it is the oldest in the ROB (ROB head points to it) and the ready/finished bit is set.
+            // Note: we can only commit 1 instruction per cycle.
+            // If store instructions --> write into memory
+            // If other instruction --> write to ARF
+            // Free ROB entry and update RAT (clear aliases). Advance ROB head to the next instruction.
+            // Mark instruction as committed (record commit cycle for table).
 
 
 
+    }
 
 
 	// Output the final results to the terminal
 	InputParser::output();
 
+	std::cout << "\nProgram End" << std::endl;
 
 
 
