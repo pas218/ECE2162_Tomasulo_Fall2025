ba90ceb23add30c74bf5e5737b7f3529ce5cd783
pas218 <pas218@pitt.edu>
2025-10-29 16:57:50 -0400

    Create Tomasulo outline.

---
+++ lib/Tomasulo.cpp
@@ -0,0 +1,35 @@
+#include "Tomasulo.hpp"
+
+bool Tomasulo::issue()
+{
+	bool success = 0;
+	
+	return success;
+}
+bool Tomasulo::execute()
+{
+	bool success = 0;
+	
+	return success;
+}
+
+bool Tomasulo::mem()
+{
+	bool success = 0;
+	
+	return success;
+}
+
+bool Tomasulo::wb()
+{
+	bool success = 0;
+	
+	return success;
+}
+
+bool Tomasulo::commit()
+{
+	bool success = 0;
+	
+	return success;
+}
